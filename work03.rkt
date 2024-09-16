a6132102938111c108323d316ce35460c1c19ae6
The2070Tube <2070academy@gmail.com>
2024-09-16 18:30:40 -0400

    first commit

---
+++ work03.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define b 3)
+(define h 4)
+
+(define c 5)
+
+(define m1 6)
+(define m2 36)
+(define d 4)
+
+(define A (/ (* b h) 2))
+(define F (+ (* (/ 9 5) c) 32))
+(define G (/ (* m1 m2) (* d d)))
